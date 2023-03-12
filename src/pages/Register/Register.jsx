1e3409c56292811eeeeec8e61562ac84a12d8326
athenaweb629 <athenaweb629@gmail.com>
2023-03-12 15:11:12 +0900

    Update Register.jsx

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { Typography, Link } from "@mui/material";
-import { NotificationManager } from "react-notifications";
 import { SHInput, SHDivider, SHButton, SHCard } from "components";
 import { registerStart, clearError } from "store/Auth/authActions";
 
